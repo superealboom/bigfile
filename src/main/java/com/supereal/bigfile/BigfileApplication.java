c18dde68f124cb7d6cc4d82ecfa48de03924baa5
tianci <superealboom@163.com>
2019-01-09 16:07:50 +0800

    first commit

---
+++ src/main/java/com/supereal/bigfile/BigfileApplication.java
@@ -0,0 +1,14 @@
+package com.supereal.bigfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigfileApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/supereal/bigfile/BigfileApplicationTests.java
@@ -0,0 +1,17 @@
+package com.supereal.bigfile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BigfileApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
